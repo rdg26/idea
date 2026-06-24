5e1bb8749f21aa981759701665ca2649a02bff4e
RDG26 <rodrigo@itcnet.com.br>
2026-06-24 15:59:23 -0300

    Idea 2.2.1 - Bootstrap e Bootstrap Icons instalados

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
                 <x-form.field name="email" :label="__('auth.email')" type="email" />
                 <x-form.field name="password" :label="__('auth.password')" type="password" />
                 <button type="submit" class="btn mt-2 h-10 w-full">{{ __('auth.login_title') }}</button>
+                
 
                            
             </form>
